68aeb24f68815d2e527c3ef1d3e02ec741adfb91
Greg Moten Jr <gmotenjr@gmail.com>
2017-07-15 13:09:49 -0400

    set up

---
+++ KungFuMaster2.0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KungFuMaster2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/*The Apprentice Soon Shall Become The Master*/
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+
+
+            //END PROGRAM
+        }
+    }
+}
