e26f3f5319bec47da3b7404229a3b5d1f7e346f8
Greg Moten Jr <gmotenjr@gmail.com>
2017-07-15 13:22:17 -0400

    basic function tests

--- KungFuMaster2.0/Program.cs
+++ KungFuMaster2.0/Program.cs
@@ -10,11 +10,33 @@ namespace KungFuMaster2._0
     {
         static void Main(string[] args)
         {/*The Apprentice Soon Shall Become The Master*/
-            Console.WriteLine("Hello, World!");
-            Console.ReadKey();
+            //Console.WriteLine("Hello, World!");
+            //Console.ReadKey();
+            //END SETUP
+
+            /*BEGIN 1-FUNCTION TESTS*/
+            int a;
+            int b;
+            int c;
+
+            Console.WriteLine("Enter value of 'a':");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter value of 'b':");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            //why can't I not use it this way?
+            c = Add(a, b);
+            Console.WriteLine("a + b = {0}", c);
+        }//END   Main
+
+        static public int Add(int x, int y)
+        {
+            int result = x + y;
+            return result;
+        }//END   Add
 
 
-            //END PROGRAM
-        }
-    }
+    }// END PROGRAM
 }
+
