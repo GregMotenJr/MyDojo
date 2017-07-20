ffcdc197f47bea7e4b800687410d37d9ce07c3d9
Greg Moten Jr <gmotenjr@gmail.com>
2017-07-20 11:17:36 -0400

    started TDD, add function

--- KungFuMaster2.0/Program.cs
+++ KungFuMaster2.0/Program.cs
@@ -10,31 +10,44 @@ namespace KungFuMaster2._0
     {
         static void Main(string[] args)
         {/*The Apprentice Soon Shall Become The Master*/
-            //Console.WriteLine("Hello, World!");
-            //Console.ReadKey();
-            //END SETUP
+         //Console.WriteLine("Hello, World!");
+         //Console.ReadKey();
+         //END SETUP
 
             /*BEGIN 1-FUNCTION TESTS*/
-            int a;
-            int b;
-            int c;
+            //    int a;
+            //    int b;
+            //    int c;
 
-            Console.WriteLine("Enter value of 'a':");
-            a = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine("Enter value of 'a':");
+            //    a = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter value of 'b':");
-            b = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine("Enter value of 'b':");
+            //    b = Convert.ToInt32(Console.ReadLine());
 
-            //why can't I not use it this way?
-            c = Add(a, b);
-            Console.WriteLine("a + b = {0}", c);
-        }//END   Main
+            //    //why can't I not use it this way?
+            //    c = Add(a, b);
+            //    Console.WriteLine("a + b = {0}", c);
+            //}//END   Main
 
-        static public int Add(int x, int y)
+            //static public int Add(int x, int y)
+            //{
+            //    int result = x + y;
+            //    return result;
+            //}//END   Add
+            /*END 1-FUNCTION TESTS*/
+
+        }// END MAIN
+
+         /*BEGIN 2 - ADD */
+         //Write a function that returns the sum of two numbers.
+        int add(int param1, int param2)
         {
-            int result = x + y;
-            return result;
-        }//END   Add
+            int sum = param1 + param2;
+            return sum;
+        }// END 2
+
+        
 
 
     }// END PROGRAM
