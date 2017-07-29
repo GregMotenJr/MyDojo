35d9b917d47311f34857acbc50ce6905f0bbf95c
Greg Moten Jr <gmotenjr@gmail.com>
2017-07-28 22:02:32 -0400

    clear program and re-organize

--- KungFuMaster2.0/Program.cs
+++ KungFuMaster2.0/Program.cs
@@ -6,55 +6,18 @@ using System.Threading.Tasks;
 
 namespace KungFuMaster2._0
 {
+    // The Apprentice Soon Shall Become The Master
+    // my work space for kata practice
+
     class Program
     {
         static void Main(string[] args)
-        {/*The Apprentice Soon Shall Become The Master*/
-            ///Check to see if a string has the same amount of 'x's and 'o's. 
-            ///The method must return a boolean and be case insensitive. 
-            ///The string can contains any char.
-
-            string source = "apo";
-            //int countX = 0;
-            //int countY = 0;
-            var countX = source.ToLower().Count(x => x == 'x');
-            var countY = source.ToLower().Count(x => x == 'y');
-
-            Console.WriteLine("Their are {0} X's and {1} Y's", countX, countY);
-            //while (countX < source.ToLower().Length && source[countX] == 'x') countX++;
-            //while (countY < source.ToLower().Length && source[countY] == 'y') countY++;
-
-            if (countX == countY)
-            {
-                Console.WriteLine("X and Y are equal");
-            }
-            else
-            {
-                Console.WriteLine("X and Y are not equal");
-            }
-            Console.ReadKey();
-        }
-
-        ///the finished code
-        public static bool XO(string input)
         {
-            //Establish Variables, convert the string to lowercase then 
-            //go through the string and count the X and O chars
+            //Title:
+            ///Instructions:
 
-            var countX = input.ToLower().Count(x => x == 'x');
-            var countO = input.ToLower().Count(x => x == 'o');
 
-            //Comparing the values to see if they are equal 
-            if (countX == countO)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+        }//END Main
 
-        }
-
-    }
+    }//END Program
 }
